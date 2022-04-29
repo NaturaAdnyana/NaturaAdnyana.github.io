d359469dbedd663617ca5347faa8cc326363fe35
NaturaAdnyana <natura538@gmail.com>
2022-04-29 11:54:23 +0800

    Add lazy loading to routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import React, { Suspense } from "react";
 import "./App.css";
-import Contact from "./pages/Contact";
-import Home from "./pages/home/";
-import NotFound from "./pages/NotFound";
-import UnderContstruction from "./pages/UnderContstruction";
-import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
+const Home = React.lazy(() => import("./pages/home"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const UnderContstruction = React.lazy(() =>
+  import("./pages/UnderContstruction")
+);
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Navbar />
-      </header>
-      <main className="scroll-smooth">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="works" element={<UnderContstruction />} />
-          <Route path="blogs" element={<UnderContstruction />} />
-          <Route path="certifications" element={<UnderContstruction />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+      <Suspense fallback={<span>Loading. . .</span>}>
+        <header>
+          <Navbar />
+        </header>
+        <main className="scroll-smooth">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="works" element={<UnderContstruction />} />
+            <Route path="blogs" element={<UnderContstruction />} />
+            <Route path="certifications" element={<UnderContstruction />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </Suspense>
     </div>
   );
 }
