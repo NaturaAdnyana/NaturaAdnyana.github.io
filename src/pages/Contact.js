f1c6c912ce7fd1d9fb6ab7e2f2dcad747c3e7f55
NaturaAdnyana <natura538@gmail.com>
2022-04-30 11:44:34 +0800

    Fix font size on contact & underconstruction page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Contact() {
   return (
-    <section className="text-white">
-      <article className="flex justify-center items-center">
+    <section className="text-white h-screen">
+      <article className="flex flex-col justify-center items-center text-center">
         <h1 className="mt-52">
-          <span className="text-9xl">Contact</span>
-          <span className="text-8xl">Us</span>
-          <br />
-          <span className="text-9xl font-bold"> NATURA</span>
+          <span className="text-5xl md:text-9xl">CONTACT US</span>
         </h1>
+        <p>
+          <span>Under Construction</span>
+        </p>
       </article>
     </section>
   );
--- src/pages/UnderContstruction.js
+++ src/pages/UnderContstruction.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function UnderContstruction() {
   return (
-    <section className="text-white">
+    <section className="text-white h-screen">
       <article className="flex justify-center items-center">
         <h1 className="mt-52">
-          <span className="text-9xl">UNDER</span>
+          <span className="text-5xl md:text-9xl">UNDER</span>
           <br />
-          <span className="text-9xl font-bold"> CONSTRUCTION</span>
+          <span className="text-5xl md:text-9xl font-bold"> CONSTRUCTION</span>
         </h1>
       </article>
     </section>
