b75bc713702d064e61398ccdbe0067fa0041c0b0
NaturaAdnyana <natura538@gmail.com>
2022-07-18 23:18:24 +0800

    redesign 404 not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import OutlineBtn from "../shared/OutlineBtn";
+import HomeIcon from "../assets/icons/HomeIcon";
 
 function NotFound() {
   return (
-    <section className="text-white">
-      <article className="flex justify-center items-center">
-        <h1 className="mt-52">
-          <span className="text-9xl">NOT FOUND</span>
-          <br />
-          <span className="text-9xl font-bold">
-            {" "}
-            NOTHING TO <br /> SEE HERE
-          </span>
+    <section className="text-white h-screen">
+      <article className="flex flex-col space-y-9 justify-center items-center h-full">
+        <h1 className="text-center">
+          <span className="text-10xl font-bold block glitch">404</span>
+          <span className="text-5xl md:text-9xl">NOT FOUND</span>
         </h1>
+        <Link to="/">
+          <OutlineBtn hint="Go to Home">
+            <HomeIcon className="w-6 h-6 ml-2" />
+          </OutlineBtn>
+        </Link>
       </article>
     </section>
   );
--- src/shared/OutlineBtn.js
+++ src/shared/OutlineBtn.js
@@ -7,7 +7,7 @@ function OutlineBtn({ hint, children }) {
       <motion.div
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 1 }}
-        className="group p-4 border-2 transition-all hover:bg-white hover:text-black rounded overflow-hidden text-center cursor-pointer bg-black-transparent"
+        className="group p-4 border-2 transition-all hover:bg-white hover:text-black rounded overflow-hidden cursor-pointer bg-black-transparent flex justify-center items-center"
       >
         <span>{hint}</span>
         {children}
