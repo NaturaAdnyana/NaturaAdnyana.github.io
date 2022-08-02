0de92a2fd8bfd2496fc047f35ac01bc94d4b3e56
NaturaAdnyana <natura538@gmail.com>
2022-08-02 20:41:11 +0800

    add new portfolio/work

--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion, useViewportScroll, useTransform } from "framer-motion";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import GithubIcon from "../../assets/icons/GithubIcon";
@@ -9,6 +9,8 @@ import playWithArray from "../../assets/images/works/playWithArray.jpg";
 import seputarBolaID from "../../assets/images/works/seputarBolaID.jpg";
 import todoApp from "../../assets/images/works/todoApp.jpg";
 import linkTreeNatura from "../../assets/images/works/linkTreeNatura.jpg";
+import mainWork from "../../assets/images/works/mainWork.jpg";
+import mainWorkMini from "../../assets/images/works/mainWorkMini.jpg";
 
 const containerVariants = {
   hidden: {
@@ -46,6 +48,29 @@ const contentVariants = {
 
 const works = [
   {
+    id: 1,
+    name: "Natura Personal Website",
+    year: 2022,
+    url: "https://www.naturaadnyana.com",
+    github: "https://github.com/NaturaAdnyana/NaturaAdnyana.github.io",
+    techs: [
+      "React",
+      "React Router",
+      "Tailwind",
+      "Framer Motion",
+      "Axios",
+      "Strapi",
+      "Google Analytics",
+      "Cloudinary",
+      "Cloudflare",
+      "Vercel (Frontend)",
+      "Heroku (Backend)",
+    ],
+    img: mainWork,
+    smallImg: mainWorkMini,
+  },
+  {
+    id: 2,
     name: "Bali Waffle",
     year: 2020,
     url: "https://bali-waffle.netlify.app/",
@@ -53,8 +78,10 @@ const works = [
       "https://github.com/NaturaAdnyana/NaturaAdnyana-BaliWaffleHomepage-PracticingBootstrapAndSass",
     techs: ["HTML", "SASS", "Bootstrap", "Webpack"],
     img: baliWaffle,
+    smallImg: "",
   },
   {
+    id: 3,
     name: "Cari Resto",
     year: 2021,
     url: "https://find-my-restaurant.netlify.app/",
@@ -70,56 +97,69 @@ const works = [
       "Fetch API",
     ],
     img: findYourRestaurant,
+    smallImg: "",
   },
   {
+    id: 4,
     name: "Play With Array",
     year: 2021,
     url: "https://bermain-array-of-objects.netlify.app/",
     github: "https://github.com/NaturaAdnyana/BermainDenganArrayofObjects-JS",
     techs: ["HTML", "CSS", "JS"],
     img: playWithArray,
+    smallImg: "",
   },
   {
+    id: 5,
     name: "Seputar Bola",
     year: 2020,
     url: "https://seputar-bola-id.web.app/",
     github: "https://github.com/NaturaAdnyana/SeputarBola-PusatInformasiBola",
     techs: ["HTML", "CSS", "JS", "Webpack", "PWA", "Fetch API"],
     img: seputarBolaID,
+    smallImg: "",
   },
   {
+    id: 6,
     name: "Todo App",
     year: 2021,
     url: "https://personal-todo-app.vercel.app/",
     github: "https://github.com/NaturaAdnyana/FirstReactApp-TodoApp",
     techs: ["React", "CSS"],
     img: todoApp,
+    smallImg: "",
   },
   {
+    id: 7,
     name: "Natura's Linktree",
     year: 2021,
     url: "https://links.naturaadnyana.com/",
     github: "https://github.com/NaturaAdnyana/Selfmade-Likntree",
     techs: ["HTML", "CSS", "Webpack"],
     img: linkTreeNatura,
+    smallImg: "",
   },
 ];
 
 const techColor = (tech) => {
-  if (tech === "HTML") {
+  if (tech === "HTML" || tech === "React Router") {
     return "border-red-500 text-red-500";
   } else if (tech === "CSS") {
     return "border-blue-500 text-blue-500";
-  } else if (tech === "JS") {
+  } else if (tech === "JS" || tech === "Cloudflare") {
     return "border-orange-500 text-orange-500";
   } else if (tech === "SASS") {
     return "border-pink-500 text-pink-500";
-  } else if (tech === "Bootstrap") {
+  } else if (tech === "Bootstrap" || tech === "Strapi") {
     return "border-purple-500 text-purple-500";
-  } else if (tech === "React") {
+  } else if (tech === "React" || tech === "Heroku (Backend)") {
     return "border-cyan-500 text-cyan-500";
   } else if (tech === "Tailwind") {
     return "border-teal-500 text-teal-500";
+  } else if (tech === "Cloudinary") {
+    return "border-blue-500 text-blue-500";
+  } else if (tech === "Google Analytics") {
+    return "border-yellow-500 text-yellow-500";
   } else {
     return "border-gray-300 text-gray-300";
   }
@@ -128,6 +168,11 @@ const techColor = (tech) => {
 const Works = () => {
   const { scrollYProgress } = useViewportScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1.2]);
+  const [deviceWidth, setDeviceWidth] = useState(window.innerWidth);
+
+  window.onresize = function () {
+    setDeviceWidth(window.innerWidth);
+  };
   return (
     <HelmetProvider>
       <Helmet>
@@ -172,63 +217,139 @@ const Works = () => {
                   </div>
                 </div>
                 <div className="lg:p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                  {works.map(({ name, year, url, github, techs, img }) => {
-                    return (
-                      <motion.div
-                        key={name}
-                        className="group relative rounded-md overflow-hidden transition-all border"
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1 }}
-                        viewport={{ once: true }}
-                        transition={{ delay: 0.5 }}
-                      >
-                        <div className="absolute p-5 z-10">
-                          <div className="mb-5 opacity-50 text-xs flex flex-wrap">
-                            {techs.map((tech) => {
-                              return (
-                                <div
-                                  className={`px-2 py-1 mr-2 mt-2 border ${techColor(
-                                    tech
-                                  )} rounded-lg`}
-                                >
-                                  {tech}
-                                </div>
-                              );
-                            })}
+                  {works.map(
+                    ({ id, name, year, url, github, techs, img, smallImg }) => {
+                      return id === 1 ? (
+                        <motion.div
+                          key={name}
+                          className="group md:col-span-2 lg:col-span-3 relative rounded-md overflow-hidden transition-all border"
+                          initial={{ opacity: 0 }}
+                          whileInView={{ opacity: 1 }}
+                          viewport={{ once: true }}
+                          transition={{ delay: 0.5 }}
+                        >
+                          <div className="absolute p-5 z-10">
+                            {deviceWidth < 768 && (
+                              <div className="mb-5 opacity-50 text-xs flex flex-wrap">
+                                {techs.map((tech) => {
+                                  return (
+                                    <div
+                                      className={`px-2 py-1 mr-2 mt-2 border ${techColor(
+                                        tech
+                                      )} rounded-lg`}
+                                    >
+                                      {tech}
+                                    </div>
+                                  );
+                                })}
+                              </div>
+                            )}
+                            <h3 className="lg:mt-5 font-oswald text-5xl">
+                              {name}
+                            </h3>
+                            <span className="block opacity-50 text-sm mt-1">
+                              - {year}
+                            </span>
+                            {deviceWidth > 768 && (
+                              <div className="mb-5 opacity-50 text-xs flex flex-wrap w-1/3">
+                                {techs.map((tech) => {
+                                  return (
+                                    <div
+                                      className={`px-2 py-1 mr-2 mt-2 border ${techColor(
+                                        tech
+                                      )} rounded-lg`}
+                                    >
+                                      {tech}
+                                    </div>
+                                  );
+                                })}
+                              </div>
+                            )}
                           </div>
-                          <h3 className="font-oswald text-5xl">{name}</h3>
-                          <span className="block opacity-50 text-sm mt-1">
-                            - {year}
-                          </span>
-                        </div>
-                        <img
-                          className="transition-transform group-hover:scale-110 brightness-95 group-hover:brightness-75"
-                          src={img}
-                          alt=""
-                        />
-                        <div className="transition-opacity lg:opacity-0 lg:group-hover:opacity-100 absolute p-1 w-full bottom-10 flex justify-around">
-                          <a
-                            href={github}
-                            className="flex border border-white py-2 px-3 bg-black-transparent hover:bg-black rounded-lg"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <GithubIcon className="w-5 mr-1" />
-                            Github
-                          </a>
-                          <a
-                            href={url}
-                            className="flex border border-white py-2 px-3 bg-black-transparent hover:bg-black rounded-lg"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <GlobeIcon className="w-6 mr-1" />
-                            Website
-                          </a>
-                        </div>
-                      </motion.div>
-                    );
-                  })}
+                          <img
+                            className="transition-transform group-hover:scale-110 brightness-95 group-hover:brightness-75"
+                            src={deviceWidth > 768 ? img : smallImg}
+                            alt=""
+                          />
+                          <div className="transition-opacity lg:opacity-0 lg:group-hover:opacity-100 absolute p-1 w-full bottom-10 flex justify-around">
+                            <a
+                              href={github}
+                              className="flex border border-white py-2 px-3 bg-black-transparent hover:bg-black rounded-lg"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <GithubIcon className="w-5 mr-1" />
+                              Github
+                            </a>
+                            <a
+                              href={url}
+                              className="flex border border-white py-2 px-3 bg-black-transparent hover:bg-black rounded-lg"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <GlobeIcon className="w-6 mr-1" />
+                              Website
+                            </a>
+                          </div>
+                        </motion.div>
+                      ) : (
+                        <motion.div
+                          key={name}
+                          className="group relative rounded-md overflow-hidden transition-all border"
+                          initial={{ opacity: 0 }}
+                          whileInView={{ opacity: 1 }}
+                          viewport={{ once: true }}
+                          transition={{ delay: 0.5 }}
+                        >
+                          <div className="absolute p-5 z-10">
+                            <div className="mb-5 opacity-50 text-xs flex flex-wrap">
+                              {techs.map((tech) => {
+                                return (
+                                  <div
+                                    className={`px-2 py-1 mr-2 mt-2 border ${techColor(
+                                      tech
+                                    )} rounded-lg`}
+                                  >
+                                    {tech}
+                                  </div>
+                                );
+                              })}
+                            </div>
+                            <h3 className="font-oswald text-5xl">{name}</h3>
+                            <span className="block opacity-50 text-sm mt-1">
+                              - {year}
+                            </span>
+                          </div>
+                          <img
+                            className="transition-transform group-hover:scale-110 brightness-95 group-hover:brightness-75"
+                            src={img}
+                            alt=""
+                          />
+                          <div className="transition-opacity lg:opacity-0 lg:group-hover:opacity-100 absolute p-1 w-full bottom-10 flex justify-around">
+                            <a
+                              href={github}
+                              className="flex border border-white py-2 px-3 bg-black-transparent hover:bg-black rounded-lg"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <GithubIcon className="w-5 mr-1" />
+                              Github
+                            </a>
+                            <a
+                              href={url}
+                              className="flex border border-white py-2 px-3 bg-black-transparent hover:bg-black rounded-lg"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <GlobeIcon className="w-6 mr-1" />
+                              Website
+                            </a>
+                          </div>
+                        </motion.div>
+                      );
+                    }
+                  )}
+                  ;
                 </div>
               </div>
             </article>
